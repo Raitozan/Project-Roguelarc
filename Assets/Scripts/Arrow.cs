dbe4c17bd0792d98df4517f0ad057fcd05cfc4af
Desages Nathan <nathan.desages@gmail.com>
2020-03-04 22:25:47 +0100

    arrow lifetime

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
 	Vector3 previousPos;
+	public float lifetime;
 	public LayerMask hitmask;
 	float speed;
 
@@ -19,17 +20,17 @@ public class Arrow : MonoBehaviour
 		if(speed > 0.0f)
 		{
 			//move
+			previousPos = transform.position;
 			Vector3 velocity = transform.forward * speed;
 			transform.position = transform.position + velocity * Time.deltaTime;
 
-			//check high speed collision
+			//check collision
 			Vector3 toNewPos = transform.position - previousPos;
 			RaycastHit hit;
 			if (Physics.Raycast(previousPos, toNewPos.normalized, out hit, toNewPos.magnitude, hitmask))
-			{
 				HandleCollision(hit.collider);
-			}
-			previousPos = transform.position;
+			else if ((lifetime -= Time.deltaTime) <= 0.0f)
+				StartCoroutine(DestroyCoroutine());
 		}
 	}
 
@@ -44,7 +45,7 @@ public class Arrow : MonoBehaviour
 	IEnumerator DestroyCoroutine()
 	{
 		speed = 0.0f;
-		yield return new WaitForSeconds(0.5f);
+		yield return new WaitForSeconds(0.1f);
 		Destroy(gameObject);
 	}
 }
